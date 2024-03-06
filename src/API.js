b04f58384d0eb10002a2fd0fa7968ff281163d90
Ronier Ramos <ronier2332@gmail.com>
2024-03-06 22:33:37 +0100

    add axios instance and interceptor for headers

--- src/API.js
+++ src/API.js
@@ -1,6 +1,14 @@
 import axios from 'axios'
 
 export const API = axios.create({
-    baseURL: 'https://localhost:3000',
-    timeout: 1000,
+    baseURL: 'http://localhost:3000',
+    timeout: 1000
   });
+
+// Add a request interceptor to set content type headers
+axios.interceptors.request.use(function (config) {
+  const headers = "application/json"
+  config.headers['Content-Type'] =  headers;
+
+  return config;
+});
