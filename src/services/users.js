689b0aa14b1db8c104f72c8987f0ea1009f96224
Ronier Ramos <ronier2332@gmail.com>
2024-03-08 01:23:35 +0100

    filter by user name and role

---
+++ src/services/users.js
@@ -0,0 +1,13 @@
+import {API} from "../API.js";
+
+const config = {
+    headers: {
+        'x-access-token': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjoidGVzdCIsImlhdCI6MTcwOTgxMDg4NSwiZXhwIjoxNzA5ODE0NDg1fQ.XcoNIAVY1ne-0ObgCqKFFzdbVEpIo2n6XnAa85Uyavo'
+    }
+}
+
+export const getUsers = async () => await API.get('/users', config)
+
+export const deleteUser = async (id) => await API.delete(`/users/${id}`, config)
+
+export const addUser = async ({user}) => await API.put('/users', user, config)
