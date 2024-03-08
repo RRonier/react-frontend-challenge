09f2d587518ed296ad4c0a36dcd581214ff5e6e7
Ronier Ramos <ronier2332@gmail.com>
2024-03-08 15:09:18 +0100

    add react internacionalization config

---
+++ src/i18n.js
@@ -0,0 +1,49 @@
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+
+const resources = {
+    en: {
+        translation: {
+            "login": "Login",
+            "userLabel": "User",
+            "passwordLabel": "Password",
+            "userRequiredError": "The user is required",
+            "passwordRequiredError": "The password is required",
+            "usersData": "Users Data",
+            "tableSubtitle": "Detailed list of users in the database",
+            "filterLabel": "Filter by name or role",
+            "user": "user",
+            "email": "email",
+            "roles": "roles",
+            "actions": "actions"
+        }
+    },
+    es: {
+        translation: {
+            "login": "Iniciar sesion",
+            "userLabel": "Usuario",
+            "passwordLabel": "Contraseña",
+            "userRequiredError": "El usuario es obligatorio",
+            "passwordRequiredError": "La contraseña es obligatoria",
+            "usersData": "Datos de los Usuarios",
+            "tableSubtitle": "Lista detallada de los usuarios en la base de datos",
+            "filterLabel": "Filtrar por nombre o rol",
+            "user": "usuario",
+            "email": "correo",
+            "roles": "roles",
+            "actions": "acciones"
+        }
+    }
+};
+
+i18n
+    .use(initReactI18next)
+    .init({
+        resources,
+        lng: "en",
+        interpolation: {
+            escapeValue: false
+        }
+    });
+
+export default i18n;
