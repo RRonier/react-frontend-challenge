b63d5b68db0fd5f09063292187cdde0728a0015c
Ronier Ramos <ronier2332@gmail.com>
2024-03-21 22:42:13 +0100

    refactor auth page

--- src/pages/auth.jsx
+++ src/pages/auth.jsx
@@ -7,16 +7,16 @@ import {
     TextField,
     Typography
 } from '@mui/material';
-import {useNavigate} from "react-router-dom";
-import {useTranslation} from "react-i18next";
-import {useDispatch, useSelector} from 'react-redux';
-import {login} from '../store/slices/auth.slice.js'
+import { useNavigate } from "react-router-dom";
+import { useTranslation } from "react-i18next";
+import { useDispatch, useSelector } from 'react-redux';
+import { login } from '../store/slices/auth.slice.js';
 
 const LoginPage = () => {
     const navigate = useNavigate()
-    const {t} = useTranslation()
+    const { t } = useTranslation()
     const dispatch = useDispatch();
-    const {user} = useSelector(state => state.auth)
+    const { error, loading } = useSelector(state => state.auth)
 
     const formik = useFormik({
         initialValues: {
@@ -36,102 +36,100 @@ const LoginPage = () => {
         }),
         onSubmit: async (values, helpers) => {
             try {
-                dispatch(login(values));
-                if(user && user.user) {
-                    navigate("/dashboard", { replace: true, relative: "route" })
-                } else {
-                    helpers.setErrors({ submit: t("error_on_submit") });
-                }
+                dispatch(login(values))
+                    .then(({ payload }) => {
+                        if (payload) navigate("/dashboard")
+                    },(error) => console.log(error))
             } catch (err) {
                 helpers.setStatus({ success: false });
-                helpers.setErrors({ submit: t("error_on_submit")  });
+                helpers.setErrors({ submit: err });
                 helpers.setSubmitting(false);
             }
         }
     });
 
     return (
+        <Box
+            sx={{
+                backgroundColor: 'background.paper',
+                flex: '1 1 auto',
+                alignItems: 'center',
+                display: 'flex',
+                justifyContent: 'center',
+                width: 400,
+                margin: '0 auto'
+            }}
+        >
             <Box
                 sx={{
-                    backgroundColor: 'background.paper',
-                    flex: '1 1 auto',
-                    alignItems: 'center',
-                    display: 'flex',
-                    justifyContent: 'center',
-                    width: 400,
-                    margin: '0 auto'
+                    maxWidth: 700,
+                    px: 3,
+                    py: '100px',
+                    width: '100%'
                 }}
             >
-                <Box
-                    sx={{
-                        maxWidth: 700,
-                        px: 3,
-                        py: '100px',
-                        width: '100%'
-                    }}
-                >
-                    <Typography variant='h4' sx={{
-                        color: 'black',
-                    }}>{t('login')}</Typography>
-                    <div>
-                        <Stack
-                            spacing={1}
-                            sx={{ mb: 3 }}
-                        >
-                            <Typography variant="h4">
-                                Login
-                            </Typography>
-                        </Stack>
-                        <form
-                            noValidate
-                            onSubmit={formik.handleSubmit}
-                        >
-                            <Stack spacing={3}>
-                                <TextField
-                                    error={!!(formik.touched.user && formik.errors.user)}
-                                    fullWidth
-                                    helperText={formik.touched.user && formik.errors.user}
-                                    label={t('userLabel')}
-                                    name="user"
-                                    onBlur={formik.handleBlur}
-                                    onChange={formik.handleChange}
-                                    type="text"
-                                    value={formik.values.user}
-                                />
-                                <TextField
-                                    error={!!(formik.touched.password && formik.errors.password)}
-                                    fullWidth
-                                    helperText={formik.touched.password && formik.errors.password}
-                                    label={t('passwordLabel')}
-                                    name="password"
-                                    onBlur={formik.handleBlur}
-                                    onChange={formik.handleChange}
-                                    type="password"
-                                    value={formik.values.password}
-                                />
-                            </Stack>
-                            {formik.errors.submit && (
-                                <Typography
-                                    color="error"
-                                    sx={{ mt: 3 }}
-                                    variant="body2"
-                                >
-                                    {formik.errors.submit}
-                                </Typography>
-                            )}
-                            <Button
+                <Typography variant='h4' sx={{
+                    color: 'black',
+                }}>{t('login')}</Typography>
+                <div>
+                    <Stack
+                        spacing={1}
+                        sx={{ mb: 3 }}
+                    >
+                        <Typography variant="h4">
+                            Login
+                        </Typography>
+                    </Stack>
+                    <form
+                        noValidate
+                        onSubmit={formik.handleSubmit}
+                    >
+                        <Stack spacing={3}>
+                            <TextField
+                                error={!!(formik.touched.user && formik.errors.user)}
                                 fullWidth
-                                size="large"
-                                sx={{ mt: 3 }}
-                                type="submit"
-                                variant="contained"
+                                helperText={formik.touched.user && formik.errors.user}
+                                label={t('userLabel')}
+                                name="user"
+                                onBlur={formik.handleBlur}
+                                onChange={formik.handleChange}
+                                type="text"
+                                value={formik.values.user}
+                            />
+                            <TextField
+                                error={!!(formik.touched.password && formik.errors.password)}
+                                fullWidth
+                                helperText={formik.touched.password && formik.errors.password}
+                                label={t('passwordLabel')}
+                                name="password"
+                                onBlur={formik.handleBlur}
+                                onChange={formik.handleChange}
+                                type="password"
+                                value={formik.values.password}
+                            />
+                        </Stack>
+                        {error && (
+                            <Typography
+                                color="error"
+                                sx={{ mt: 2 }}
+                                variant="body2"
                             >
-                                {t('login')}
-                            </Button>
-                        </form>
-                    </div>
-                </Box>
+                                {error}
+                            </Typography>
+                        )}
+                        <Button
+                            fullWidth
+                            size="large"
+                            sx={{ mt: 2 }}
+                            type="submit"
+                            variant="contained"
+                        >
+                            {loading ? t("loading") : t('login')}
+                        </Button>
+                    </form>
+                </div>
             </Box>
+        </Box>
     );
 };
 
