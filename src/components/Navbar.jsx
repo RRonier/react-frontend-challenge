1d3230e1323086e76518ea04837f4e2d25e3f4eb
Ronier Ramos <ronier2332@gmail.com>
2024-03-21 22:39:43 +0100

    add logout to navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,26 +2,34 @@ import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
-import {useSelector} from "react-redux";
-import {Person} from "@mui/icons-material"
+import { useSelector, useDispatch } from "react-redux";
+import { useNavigate } from 'react-router-dom'
+import { Person } from "@mui/icons-material"
+import { logout } from '../store/slices/auth.slice.js'
 
 export default function Navbar() {
-    const {user} = useSelector(state => state.auth)
+    const { user } = useSelector(state => state.auth)
+    const dispatch = useDispatch()
+    const navigate = useNavigate()
+
     return (
         <AppBar position="static">
-            <Toolbar sx={{display: 'flex', alignItems: 'center'}}>
+            <Toolbar sx={{ display: 'flex', alignItems: 'center' }}>
                 <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
                     Users management App
                 </Typography>
-                {user && user.user &&
-                    <div style={{display: 'flex', marginRight: '25px'}}>
-                        <Person/>
+                {user &&
+                    <div style={{ display: 'flex', marginRight: '25px' }}>
+                        <Person />
                         <Typography component="div">
-                            {user.user}
+                            {user}
                         </Typography>
                     </div>
                 }
-                <Button color="inherit" onClick={() => localStorage.removeItem('token')}>Logout</Button>
+                <Button color="inherit" onClick={() => {
+                    dispatch(logout())
+                    navigate("/")
+                }}>Logout</Button>
             </Toolbar>
         </AppBar>
     );
