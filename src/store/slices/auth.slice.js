2be4b5d4d944c1a99360c2c3d15c41d1cd0f25a0
Ronier Ramos <ronier2332@gmail.com>
2024-03-21 22:39:26 +0100

    refactor auth login in redux and add logout action

--- src/store/slices/auth.slice.js
+++ src/store/slices/auth.slice.js
@@ -1,33 +1,51 @@
-import {createSlice, createAsyncThunk} from '@reduxjs/toolkit';
-import {loginService} from "../../services/auth.js"; // Assuming you have an API module
+import { createSlice, createAsyncThunk, createAction } from '@reduxjs/toolkit';
+import { API } from '../../API';
 
 export const login = createAsyncThunk(
     'users/login',
     async (userData) => {
-        const {user, password} = userData
-        const {data} = await loginService(user, password);
-        localStorage.setItem('token', data.token)
-        localStorage.setItem('user', data.user)
+        const { user, password } = userData
+        const { data } = await API.post("login", { user, password });
 
         return data;
     }
 );
 
+export const logout = createAction('users/logout');
+
 export const authSlice = createSlice({
     name: 'auth',
-    initialState: { user: null, loading: 'idle', error: null },
+    initialState: { user: null, token: null, loading: null, error: null },
+    reducers: {
+        logout: (state) => {
+            state.user = null;
+            state.token = null;
+            state.loading = null;
+            state.error = null;
+        },
+    },
     extraReducers: (builder) => {
         builder
             .addCase(login.pending, (state) => {
-                state.loading = 'loading';
+                state.loading = true;
+                state.token = null;
+                state.user = null;
+                state.error = null;
             })
             .addCase(login.fulfilled, (state, action) => {
-                state.loading = 'idle';
-                state.user = action.payload;
+                state.loading = false;
+                state.user = action.payload.user;
+                state.token = action.payload.token;
+                state.error = null;
             })
             .addCase(login.rejected, (state, action) => {
-                state.loading = 'idle';
-                state.error = action.error.message;
+                state.loading = false;
+                state.user = null;
+                if (action.error.message === "Request failed with status code 401") {
+                    state.error = "Access Denied! Invalid Credentials";
+                } else {
+                    state.error = action.error.message;
+                }
             });
     },
 });
