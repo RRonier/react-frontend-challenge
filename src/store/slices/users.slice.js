7ce80bbe638aad44773cf311dfb77d5b0861ca2b
Ronier Ramos <ronier2332@gmail.com>
2024-03-22 23:01:47 +0100

    add update user slice and refactor user slice

--- src/store/slices/users.slice.js
+++ src/store/slices/users.slice.js
@@ -1,25 +1,27 @@
-import {createAsyncThunk, createSlice} from '@reduxjs/toolkit';
-import {addUser, deleteUser, getUsers} from "../../services/users.js";
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import { addUser, deleteUser, getUsers, editUser } from "../../services/users.js";
 
 export const fetchUsers = createAsyncThunk(
     'users/fetchUsers',
     async (token) => {
-        const {data} = await getUsers(token);
+        const { data } = await getUsers(token);
         return data;
     }
 )
 
 export const removeUser = createAsyncThunk(
     'users/deleteUser',
-    async (id, token) => {
-        const {data} = await deleteUser(id, token)
+    async (values) => {
+        const { data } = await deleteUser(values.id, values.token)
+
         return data
     })
 
 export const createUser = createAsyncThunk(
     'users/createUser',
-    async (values, token) => {
-        const {name, email, roles} = values
+    async (values) => {
+        const { name, email, roles, token } = values
+
         return await addUser(
             name,
             email,
@@ -28,44 +30,73 @@ export const createUser = createAsyncThunk(
         )
     })
 
+export const updateUser = createAsyncThunk(
+    'users/updateUser',
+    async (values) => {
+        const { name, email, roles, id, token } = values;
+
+        return await editUser(
+            id,
+            name,
+            email,
+            roles,
+            token
+        );
+    }
+)
+
 const usersSlice = createSlice({
     name: 'users',
-    initialState: { entities: [], loading: 'idle' },
+    initialState: { entities: [], loading: false },
     reducers: {},
     extraReducers: (builder) => {
         builder
             .addCase(fetchUsers.pending, (state) => {
-                state.loading = 'loading';
+                state.loading = true;
             })
             .addCase(fetchUsers.fulfilled, (state, action) => {
-                state.loading = 'idle';
+                state.loading = false;
                 state.entities = action.payload;
             })
             .addCase(fetchUsers.rejected, (state, action) => {
-                state.loading = 'idle';
+                state.loading = false;
                 state.error = action.payload;
             })
             .addCase(removeUser.pending, (state) => {
-                state.loading = 'loading';
+                state.loading = true;
             })
             .addCase(removeUser.fulfilled, (state, action) => {
-                state.loading = 'idle';
+                state.loading = false;
                 state.entities = state.entities.filter((user) => user.id !== action.payload.id);
             })
             .addCase(removeUser.rejected, (state, action) => {
-                state.loading = 'idle';
+                state.loading = false;
                 state.error = action.payload;
             })
             .addCase(createUser.pending, (state) => {
-            state.loading = 'loading';
+                state.loading = true;
             })
             .addCase(createUser.fulfilled, (state, action) => {
-                state.loading = 'idle';
+                state.loading = false;
                 state.entities = [...state.entities, action.payload.data]
             })
             .addCase(createUser.rejected, (state, action) => {
-                state.loading = 'idle';
+                state.loading = false;
                 state.error = action.payload;
+            })
+            .addCase(updateUser.pending, (state) => {
+                state.loading = true;
+            })
+            .addCase(updateUser.fulfilled, (state, action) => {
+                state.loading = false;
+                const index = state.entities.findIndex((user) => user.id === action.payload.data.id);
+                if (index !== -1) {
+                    state.entities[index] = action.payload.data;
+                }
+            })
+            .addCase(updateUser.rejected, (state, action) => {
+                state.loading = false;
+                state.error = "there's an error";
             });
     },
 });
