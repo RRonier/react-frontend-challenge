dcf31fe27f451a6ffcf3f1105c8d3dbdc80bfd6a
Ronier Ramos <ronier2332@gmail.com>
2024-03-22 23:00:49 +0100

    add token to user services

--- src/services/users.js
+++ src/services/users.js
@@ -1,23 +1,22 @@
 import { API } from "../API.js";
 
 export const getUsers = async (token) => await API.get('/users', {
-    headers: {
-        'x-access-token': `Bearer ${token}`
-    }
+    headers: { 'x-access-token': token }
 })
 
 export const deleteUser = async (id, token) => await API.delete(`/users/${id}`, {
-    headers: {
-        'x-access-token': `Bearer ${token}`
-    }
+    headers: { 'x-access-token': token }
 })
 
 export const addUser = async (name, email, roles, token) => await API.post('/users', {
     name,
     email,
     roles
-}, {
-    headers: {
-        'x-access-token': `Bearer ${token}`
-    }
-})
+}, { headers: { 'x-access-token': token } })
+
+export const editUser = async (id, name, email, roles, token) => await API.put(`/users/${id}`, {
+    id,
+    name,
+    email,
+    roles
+}, { headers: { 'x-access-token': token } })
