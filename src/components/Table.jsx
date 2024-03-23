039cb31484c4a6d390f3666f44ba98e72ab46e4a
Ronier Ramos <ronier2332@gmail.com>
2024-03-23 01:00:57 +0100

    add id to table data

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -27,18 +27,7 @@ function EnhancedTableHead(props) {
     return (
         <TableHead>
             <TableRow>
-                <TableCell padding="checkbox">
-                    <Checkbox
-                        color="primary"
-                        indeterminate={numSelected > 0 && numSelected < rowCount}
-                        checked={rowCount > 0 && numSelected === rowCount}
-                        onChange={onSelectAllClick}
-                        inputProps={{
-                            'aria-label': 'select all desserts',
-                        }}
-                    />
-                </TableCell>
-                {[{ label: t('user').toUpperCase() }, { label: t('email').toUpperCase() }, { label: t('roles').toUpperCase() }, { label: t('actions').toUpperCase() }].map((headCell, i) => (
+                {[{ label: 'id'.toUpperCase() }, { label: t('user').toUpperCase() }, { label: t('email').toUpperCase() }, { label: t('roles').toUpperCase() }, { label: t('actions').toUpperCase() }].map((headCell, i) => (
                     <TableCell
                         key={i}
                         align={'left'}
@@ -105,19 +94,14 @@ export const EnhancedTable = ({ users, deleteUser }) => {
                                         key={index}
                                         sx={{ cursor: 'pointer' }}
                                     >
-                                        <TableCell padding="checkbox" align="right">
+                                        <TableCell align="left">
+                                            {row.id}
                                         </TableCell>
-                                        <TableCell
-                                            component="th"
-                                            scope="row"
-                                            padding="none"
-                                            align="left"
-                                        >
+                                        <TableCell align="left">
                                             {row.name}
                                         </TableCell>
                                         <TableCell align="left">{row.email}</TableCell>
-                                        <TableCell align="left">{row.roles.join(", ")}
-                                        </TableCell>
+                                        <TableCell align="left">{row.roles.join(", ")}</TableCell>
                                         <TableCell align="left" sx={{ display: "flex", alignItems: "center", justifyContent: 'flex-start' }}>
                                             {user === 'admin' && (
                                                 <>
